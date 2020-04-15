87d792b4b63d45c5786faf472b1a80d872031b88
Devansh <devanshsharma289@gmail.com>
2020-04-15 02:09:04 -0400

    Added Bonus 1

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+/*=============================================
+=                Devansh Sharma               =
+=       Fullstack Internship Application      =
+=============================================*/
 
 
 addEventListener('fetch', event => {
   event.respondWith(fetchVariants(event.request))
 })
 
+
+// Function to fetch the two possible redirection URLs
 async function fetchVariants(request) {
 
+  //Retrieve cookie 
   const cookie = request.headers.get('cookie')
   const init = {
     method: 'GET',
@@ -53,17 +56,59 @@ async function generateVariation(urls, cookie) {
   ])
 
 
-  return modifyHTML(variant.body, val)
+  return modifyHTML(variant.body, val, variant)
 }
 
-async function modifyHTML(textBody, val) {
+async function modifyHTML(textBody, val, variant) {
   const responseInit = {
     status: 200,
     headers: { 'Content-Type': 'text/html' }
   }
-  myCookie = `variant=${val}; Expires=Wed, 21 Oct 2020 07:28:00 GMT; Path='/';`
 
-  res = new Response(textBody, responseInit)
+
+  class titleHandler {
+    element(e) {
+      e.prepend("Welcome to ");
+    }
+  }
+
+  /**
+   * Element handler to handle title of card
+   */
+  class headingHandler {
+    element(e) {
+      e.prepend("You are viewing ");
+    }
+  }
+
+  /**
+   * Element handler to handle description
+   */
+  class textHandler {
+    element(e) {
+      e.setInnerContent('Hey, My name is Devansh, and this is my Cloudflare 2020 Summer Internship Application! This web application includes all the basic funtionality as well as the bonus features!');
+    }
+  }
+
+  /**
+   * Element handler to handle link
+   */
+  class linkHandler {
+    element(e) {
+      e.setAttribute('href', 'http://devansh.site/');
+      e.setInnerContent('Check out my Portfolio');
+    }
+  }
+  let rewriter = new HTMLRewriter()
+    .on('title', new titleHandler())
+    .on('h1#title', new headingHandler())
+    .on('p#description', new textHandler())
+    .on('a#url', new linkHandler());
+
+
+  let res = new Response(rewriter.transform(variant).body, responseInit);
+
+  myCookie = `variant=${val}; Expires=Wed, 21 Oct 2020 07:28:00 GMT; Path='/';`
   res.headers.set('Set-Cookie', myCookie)
 
   return res
